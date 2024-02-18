d8e83d651305baf9c165d0834d1e3698668e952c
KDaria <dkdva@mail.ru>
2024-02-18 12:20:38 +0100

    Added a function to calculate the result

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,24 +8,56 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
-    @IBOutlet var emojiResultLabel: UILabel!
-    @IBOutlet var textResultLabel: UILabel!
     
+    // MARK: - IBOutlets
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var animalDefinitionLabel: UILabel!
+    
+    // MARK: - Public Properties
     var answersChosen: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        
+        updateResult(answersChosen)
     }
     
-//    private func getResult() -> Animal {
-//        return Animal
-//    }
+    
     @IBAction func doneButtonAction(_ sender: Any) {
         dismiss(animated: true)
     }
     
     deinit{
         print("has been deal")
+    }
+    
+    
+    private func updateResult(_ answers: [Answer]) {
+        var answerCounter: [Animal: Int] = [
+            .cat: 0,
+            .rabbit: 0,
+            .turtle: 0,
+            .dog: 0
+        ]
+        
+        answers.forEach { answer in
+            if let animalType = answerCounter[answer.animal] {
+                answerCounter.updateValue(animalType + 1, forKey: answer.animal)
+            }
+        }
+        
+        let sortedFrequencyOfAnimals = answerCounter.max { $0.value < $1.value }
+        
+        guard let mostFrequentAnimal = sortedFrequencyOfAnimals?.key else {
+            return
+        }
+        
+        updateUI(with: mostFrequentAnimal)
+    }
+    
+    private func updateUI(with animal: Animal) {
+        animalTypeLabel.text = "Вы - \(animal.rawValue)"
+        animalDefinitionLabel.text = animal.definition
     }
 }
